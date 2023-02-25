798b99d3b96ccbeeb3740f7dfa6d3b7c909fd784
Mwale Kalenga <mwale.kalenga@prosites.space>
2023-02-25 12:00:29 +0200

    Update loggedout notice.

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -14,7 +14,7 @@ export function render(type) {
   const messages = {
     saved: "This post has been saved!",
     loggedin: "Welcome! You are logged in!",
-    loggedout: "Bye for now! You are logged out!",
+    loggedout: "You are logged out!",
     updated: "This post has been updated!",
     required: "All fields are required!",
     failed: "This action failed :(",
